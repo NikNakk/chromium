838fd29a998763f33b124d1893dfdd34618b7640
Nick Kennedy <nick@nick-kennedy.com>
2026-09-24 10:17:33 +0100

    Require SharedImages for macOS OpenXR transport

--- device/vr/openxr/mac/openxr_graphics_binding_metal.h
+++ device/vr/openxr/mac/openxr_graphics_binding_metal.h
@@ -31,6 +31,7 @@ class DEVICE_VR_EXPORT OpenXrGraphicsBindingMetal
   int64_t GetSwapchainFormat(XrSession session) const override;
   XrResult EnumerateSwapchainImages(OpenXrCompositionLayer& layer) override;
   bool CanUseSharedImages() const override;
+  bool RequiresSharedImages() const override;
   void CleanupWithoutSubmit() override;
   gfx::Size GetMaxTextureSize() override;
   bool SetOverlayTexture(gfx::GpuMemoryBufferHandle texture,
